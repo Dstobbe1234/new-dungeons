9a89ac314672d9ea090c54a270ac16269853aca9
David Stobbe <d.stobbe1@share.epsb.ca>
2022-12-07 13:27:46 -0700

    binary space partitioning

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const cnv = document.getElementById("canvas");
 const ctx = cnv.getContext("2d");
-cnv.width = 500;
-cnv.height = 500;
+cnv.width = 1000;
+cnv.height = 1000;
 
 function randomInt(min, max) {
   return Math.floor(Math.random() * (max - min) + min);
@@ -10,66 +10,68 @@ function randomInt(min, max) {
 
 let spaces = [[0, 0, cnv.width, cnv.height]];
 
-function divide() {
-  for (let i = 0; i < 5; i++) {
-    const parent = spaces[i];
-    spaces.length = spaces.length * 2;
-    let direction = randomInt(0, 2);
-    console.log(parent)
-    console.log(direction)
-    
-    // console.log(parent[direction])
-    // let randomDivide = randomInt(
-    //     parent[direction] + 75,
-    //     parent[direction + 2] + parent[direction] - 75
-    // )
-    // if(parent[direction] >= 75 * 3) {
-    //     for (let j = 0; j < spaces.length; j += 2) {
-    //         if(direction == 0) {
-    //             spaces[j] = [parent[0], parent[1], randomDivide - parent[0], parent[3]];
-    //             spaces[j + 1] = [randomDivide, parent[1], parent[2] - (randomDivide - parent[0]), parent[3]];
-    //         } else {
-    //             spaces[j] = [parent[0], parent[1], parent[2], randomDivide - parent[1]];
-    //             spaces[j + 1] = [parent[0], randomDivide, parent[2], parent[3] - (randomDivide - parent[1])];
-    //         }
-    //     }
-    //}
-    // let currentSpaces = [];
-    // for(let j = 0; j < spaces.length; j++) {
-    //     currentSpaces.push(spaces[j])
-    // }
-    // for (let k = 0; k > currentSpaces.length; k++) {
-    //   let direction = randomInt(0, 2);
-    //   if (currentSpaces[i][direction + 2] >= 75 * 3) {
-    //     let randomDivide = randomInt(
-    //       currentSpaces[i][direction] + 75,
-    //       currentSpaces[i][direction + 2] + spaces[i][direction] - 75
-    //     );
-    //   }
-    //   if(direction == 0) {
-    //      spaces.push([currentSpaces[0], currentSpaces[1], randomDivide - currentSpaces[0], currentSpaces[3]])
-    //      spaces.push(randomDivide, currentSpaces[1], currentSpaces[2] + currentSpaces, currentSpaces[3]])
-    //   }
-    // }
+class dungeon {
+  constructor(x, y, w, h) {
+    this.x = x;
+    this.y = y;
+    this.w = w;
+    this.h = h;
+  } draw() {
+    ctx.fillStyle = "red"
+    ctx.fillRect(this.x, this.y, this.w, this.h)
   }
 }
-//test
 
-divide()
-
-
-for(i = 0; i < 5; i++) {
-    let newSpaces = 0
-    for(let j = 0; j < spaces.length; j++) {
-        const parent = spaces[j]
-        let direction = randomInt(0, 2);
-        let randomDivide = randomInt(
-        parent[direction] + 75,
-        parent[direction + 2] + parent[direction] - 75
-        )
-        if(parent[direction + 2] < 3 * 75) {
-            newSpaces.push([currentSpaces[0], currentSpaces[1], randomDivide - currentSpaces[0], currentSpaces[3]])
-            newSpaces.push([randomDivide, currentSpaces[1], currentSpaces[2] + currentSpaces, currentSpaces[3]])
+function divide() {
+  // for(let i = 0; i < 5; i++) {
+    let newSpaces = []
+    for(j = 0; j < spaces.length; j++) {
+      const parent = spaces[j]
+      const direction = randomInt(0, 2)
+      const randomDivide = randomInt(parent[direction] + 75, parent[direction] + parent[direction + 2] - 75)
+      console.log(parent[direction + 2])
+      if(parent[direction + 2] >= 75 * 3) {
+        if(direction == 0) {
+          newSpaces.push([parent[0], parent[1], randomDivide - parent[0], parent[3]])
+          newSpaces.push([randomDivide, parent[1], parent[2] - (randomDivide - parent[0]), parent[3]])
+        } else {
+          newSpaces.push([parent[0], parent[1], parent[2], randomDivide - parent[1]])
+          newSpaces.push([parent[0], randomDivide, parent[2], parent[3] - (randomDivide - parent[1])])
         }
+      } else {
+        newSpaces.push(parent)
+      }
     }
+    if(JSON.stringify(spaces) == JSON.stringify(newSpaces)) {
+      clearInterval(divideInterval)
+      createDungeons()
+    }
+    spaces = newSpaces
+    for(let i = 0; i < spaces.length; i++) {
+      ctx.strokeStyle = "black"
+      ctx.strokeRect(spaces[i][0], spaces[i][1], spaces[i][2], spaces[i][3])
+    }
+  // }
 }
+
+let dungeons = []
+function createDungeons() {
+  for(let i = 0; i < spaces.length; i++) {
+    let randX = randomInt(spaces[i][0] + 50, spaces[i][0] + spaces[i][2] - 50)
+    let randY = randomInt(spaces[i][1] + 10, spaces[i][1] + spaces[i][3] - 50)
+
+    if(randX % 2 !== 0) {
+      randX++
+    } 
+    if(randY % 2 !== 0) {
+      randY ++
+    }
+
+    dungeons.push(new dungeon(randX, randY, 10, 10))
+  }
+  for(let j = 0; j < dungeons.length; j++) {
+    dungeons[j].draw()
+  }
+}
+
+const divideInterval = setInterval(function() {divide() }, 1000)
